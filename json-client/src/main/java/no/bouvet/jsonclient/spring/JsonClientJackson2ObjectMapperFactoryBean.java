d8b051fe37272b46f58b2860c9ec96d5d5ebb14f
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2014-10-27 06:59:43 +0100

    Added JsonClientJackson2ObjectMapperFactoryBean again

--- json-client/src/main/java/no/bouvet/jsonclient/spring/JsonClientJackson2ObjectMapperFactoryBean.java
+++ json-client/src/main/java/no/bouvet/jsonclient/spring/JsonClientJackson2ObjectMapperFactoryBean.java
@@ -1,4 +1,16 @@
 package no.bouvet.jsonclient.spring;
 
-public class JsonClientJackson2ObjectMapperFactoryBean {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
+
+public class JsonClientJackson2ObjectMapperFactoryBean extends Jackson2ObjectMapperFactoryBean {
+
+    public JsonClientJackson2ObjectMapperFactoryBean() {
+        setIndentOutput(true);
+        afterPropertiesSet();
+        ObjectMapper objectMapper = getObject();
+        objectMapper.registerModule(new JodaModule());
+    }
+
 }
